7096683663b425e3ac0493c641b19033746188cd
Umherirrender <umherirrender_de.wp@web.de>
2017-04-17 22:15:19 +0200

    Add stylelint for css files
    Fixed the following rules:
    color-named
    font-family-name-quotes
    selector-list-comma-newline-after
    color-hex-case
    indentation
    
    Change-Id: If6b1fa9a68449d8bf8d3bfd1a4da97f2200c51d1

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,17 +9,25 @@ module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
 				'**/*.json',
+				'.stylelintrc',
+				'!node_modules/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
 				'!node_modules/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jsonlint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
