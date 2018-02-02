1c53c78a9d90786f3391f457a927289037d13c34
Umherirrender <umherirrender_de.wp@web.de>
2018-02-02 22:57:23 +0100

    Require MediaWiki 1.29 in extension.json for manifest_version 2
    manifest_version 2 is a new feature in 1.29
    (Iea5ba589c70958db7500cf3587b5ebd738532026)
    
    Change-Id: I11a946239fde18781107bb3ea49a278093c6beb8

--- JsonConfig.php
+++ JsonConfig.php
@@ -23,5 +23,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	); */
 	return;
 } else {
-	die( 'This version of the JsonConfig extension requires MediaWiki 1.25+' );
+	die( 'This version of the JsonConfig extension requires MediaWiki 1.29+' );
 }
