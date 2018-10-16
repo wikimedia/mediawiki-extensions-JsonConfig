cb3f1afc09da4834ce771433eae8bd4d2ef72fc6
Kunal Mehta <legoktm@member.fsf.org>
2018-10-16 11:31:30 -0700

    Warn if legacy PHP entry point is used
    Bug: T140851
    Change-Id: Ie8bd6fba99bb95b3e9d87e3849d369ded36d578e

--- JsonConfig.php
+++ JsonConfig.php
@@ -16,11 +16,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['JsonConfig'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['JsonConfigNamespaces'] = __DIR__ . '/JsonConfig.namespaces.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for JsonConfig extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the JsonConfig extension requires MediaWiki 1.29+' );
