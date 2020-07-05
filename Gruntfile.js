16a3e6abe50463d4f349495dc5657daab856b678
Ed Sanders <esanders@wikimedia.org>
2020-07-05 21:59:59 +0100

    build: Update eslint-config-wikimedia to 0.16.2
    Change-Id: Iad8cf21be23d0900284ef9d6ce64c974149543d5

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,14 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
-		// eslint-disable-next-line no-restricted-properties
+		// eslint-disable-next-line es/no-object-assign
 		banana: Object.assign( {
 			options: { requireLowerCase: false }
 		}, conf.MessagesDirs ),
 		eslint: {
 			options: {
-				extensions: [ '.js', '.json' ],
-				cache: true
+				cache: true,
+				fix: grunt.option( 'fix' )
 			},
 			all: [
 				'**/*.{js,json}',
