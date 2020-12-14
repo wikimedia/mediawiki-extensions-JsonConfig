0be965c8744fd3972b3589cf01ef174a9bea8d85
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-12-14 11:56:33 +0100

    Remove comments that literally repeat what the code says
    Such comments don't add anything.
    
    Change-Id: I04b2db550cd406999f0f2e49d1f1048f8401b9d7

--- includes/JCContent.php
+++ includes/JCContent.php
@@ -156,8 +156,8 @@ class JCContent extends \TextContent {
 
 	/**
 	 * Beautifies JSON prior to save.
-	 * @param Title $title Title
-	 * @param \User $user User
+	 * @param Title $title
+	 * @param \User $user
 	 * @param \ParserOptions $popts
 	 * @return JCContent
 	 */
