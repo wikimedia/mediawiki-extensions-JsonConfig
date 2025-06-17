wikimedia/mediawiki-extensions-JsonConfig

commit 02f0ee3d66e1ff47936f66b59d5142907525ce53

JCCache: Use WANObjectCache::getWithSetCallback() instead of set/get
The usage pattern of WANObjectCache in this extension is similar to
what getWithSetCallback() already does. Migrate the code to use the
getWithSetCallback() method instead.

This is step 1 of refactoring some code in this extension.

Change-Id: Ic2e838f6f96e0ade423c918060fa02247c46ca84

includes/JCCache.php
@@ -54,50 +54,60 @@ class JCCache {
 	 * @return string|JCContent|false Content string/object or false if irretrievable.
 	 */
 	public function get() {
-		if ( $this->content === null ) {
-			$value = $this->memcGet(); // Get content from the memcached
-			if ( $value === false ) {
-				if ( $this->titleValue->getConfig()->store ) {
-					$this->loadLocal(); // Get it from the local wiki
-				} else {
-					$this->loadRemote(); // Get it from HTTP
+		if ( $this->content ) {
+			return $this->content;
+		}
+
+		if ( $this->disableJsonConfigCache() ) {
+			return $this->fetchContent();
+		}
+
+		return $this->cache->getWithSetCallback(
+			$this->key,
+			$this->cacheExpiration,
+			function ( $oldValue, &$ttl ) {
+				$content = $this->fetchContent() ?: '';
+
+				// If the content is invalid, store an empty string to prevent repeated attempts
+				if ( !$content ) {
+					$ttl = 10;
 				}
-				$this->memcSet(); // Save result to memcached
-			} elseif ( $value === '' ) {
-				$this->content = false; // Invalid ID was cached
-			} else {
-				$this->content = $value; // Content was cached
+
+				if ( !is_string( $content ) ) {
+					$content = $content->getText();
+				}
+
+				return $content;
 			}
+		);
+	}
+
+	/**
+	 * Returns true if caching is disabled for JsonConfig
+	 *
+	 * TODO: Use this method in upcoming refactorings.
+	 *
+	 * @return bool
+	 */
+	private function disableJsonConfigCache(): bool {
+		return MediaWikiServices::getInstance()->getMainConfig()->get( 'JsonConfigDisableCache' );
+	}
+
+	/**
+	 * Retrieves the content of the JSON config if the page
+	 * exists on the local wiki or over HTTP and set the content
+	 * property (on-demand).
+	 *
+	 * @return string|JCContent|false
+	 */
+	private function fetchContent() {
+		if ( $this->titleValue->getConfig()->store ) {
+			$this->loadLocal(); // Get it from the local wiki
+		} else {
+			$this->loadRemote(); // Get it from HTTP
 		}
 
 		return $this->content;
-	}
-
-	/**
-	 * Retrieves content from memcached.
-	 * @return string|bool Carrier config or false if not in cache.
-	 */
-	private function memcGet() {
-		return MediaWikiServices::getInstance()->getMainConfig()->get( 'JsonConfigDisableCache' ) ?
-			false :
-			$this->cache->get( $this->key );
-	}
-
-	/**
-	 * Store $this->content in memcached.
-	 * If the content is invalid, store an empty string to prevent repeated attempts
-	 */
-	private function memcSet() {
-		if ( !MediaWikiServices::getInstance()->getMainConfig()->get( 'JsonConfigDisableCache' ) ) {
-			// caching an error condition for short time
-			$exp = $this->content ? $this->cacheExpiration : 10;
-			$value = $this->content ?: '';
-			if ( !is_string( $value ) ) {
-				$value = $value->getNativeData();
-			}
-
-			$this->cache->set( $this->key, $value, $exp );
-		}
 	}
 
 	/**
@@ -110,7 +120,7 @@ class JCCache {
 	 *   (either get() was called before, or it was set via ctor)
 	 */
 	public function resetCache( $updateCacheContent = null ) {
-		if ( !MediaWikiServices::getInstance()->getMainConfig()->get( 'JsonConfigDisableCache' ) ) {
+		if ( !$this->disableJsonConfigCache() ) {
 			$conf = $this->titleValue->getConfig();
 			// Delete the old value: this will propagate over WANCache
 			$this->cache->delete( $this->key );
@@ -119,7 +129,12 @@ class JCCache {
 					// @phan-suppress-next-line PhanTypeExpectedObjectPropAccess
 					$conf->store->cacheNewValue ) )
 			) {
-				$this->memcSet(); // update cache with the new value
+				$value = $this->content;
+				if ( !is_string( $value ) ) {
+					$value = $value->getText();
+				}
+
+				$this->cache->set( $this->key, $value, $this->cacheExpiration );
 			}
 		}
 	}

commit f2c482000dbc4c3b931b9d7c12d09875c0f67b84

tests: Integration tests for JCCache class
This test suite covers the 3 basic cases of JCCache::get()
^ when it returns false,
^ when it returns JCContent,
^ and when it returns string.

TODO: Expand test suite to cover more cases. Even though this
proves the failure that happen when we migrated to getWithSet().

The purpose of this is to simulate the failure caused by the patch
02f0ee3d66 (Ic2e838f6f96e0ade4) which was reverted in 9047f6cb69b5
(I74fc4502bad).

A revert revert (x2) has been made in I414c29325863acf0c22eb2b0
which will depend on this patch. The revert revert should fail until
it is fixed.

Change-Id: I83100cf3acbc4aa11fa6809f749affb30e3d4240

tests/phpunit/JCCacheTest.php
@@ -1,0 +1,70 @@
+<?php
+
+namespace JsonConfig\Tests\Integration;
+
+use HashBagOStuff;
+use JsonConfig\JCCache;
+use JsonConfig\JCContent;
+use JsonConfig\JCTitle;
+use MediaWiki\Page\WikiPage;
+use MediaWikiIntegrationTestCase;
+use Wikimedia\ObjectCache\WANObjectCache;
+
+/**
+ * @covers \JsonConfig\JCCache
+ * @group Database
+ */
+class JCCacheTest extends MediaWikiIntegrationTestCase {
+
+	private function newJCTitle( WikiPage $page ): JCTitle {
+		$config = (object)[
+			'namespace' => NS_MAIN,
+			'flaggedRevs' => null,
+			'cacheKey' => 'abc',
+			'isLocal' => true,
+			'cacheExp' => 100,
+			'store' => true,
+		];
+
+		return new JCTitle( NS_MAIN, $page->getDBkey(), $config );
+	}
+
+	public function testGetWithoutContent(): void {
+		$cache = new JCCache(
+			$this->newJCTitle( $this->getExistingTestPage( 'Foobar' ) ),
+			null
+		);
+
+		$result = $cache->get();
+		$this->assertSame(
+			'Test content for JCCacheTest-testGetWithoutContent',
+			$result
+		);
+	}
+
+	public function testGetWithContent(): void {
+		$cache = new JCCache(
+			$this->newJCTitle( $this->getExistingTestPage( 'Foobar' ) ),
+			new JCContent( '{}', 'test', false )
+		);
+
+		$result = $cache->get();
+
+		$this->assertInstanceOf( JCContent::class, $result );
+		$this->assertSame( '{}', $result->getText() );
+	}
+
+	public function testGetWithInvalidCache(): void {
+		$wanCache = new WANObjectCache( [ 'cache' => new HashBagOStuff() ] );
+		$this->setService( 'MainWANObjectCache', $wanCache );
+
+		$cache = new JCCache(
+			$this->newJCTitle( $this->getNonexistingTestPage( 'Foobar' ) ),
+			null
+		);
+
+		$result = $cache->get();
+		$this->assertFalse( $result );
+	}
+
+}