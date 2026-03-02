8932e61b9f8e205e45f5c4a0bcdaf0461fa94273
Umherirrender <umherirrender_de.wp@web.de>
2026-03-02 21:15:11 +0100

    tests: Skip JCLuaLibrary tests when LuaEngineTestBase is missing
    Scribunto is a optional dependency,
    not all tests jobs running with Scribunto
    
    Change-Id: I0484ac8039d45cf03f6ff4ddb2ab702972d1c5fd

--- tests/phpunit/JCLuaLibraryStandaloneTest.php
+++ tests/phpunit/JCLuaLibraryStandaloneTest.php
@@ -2,6 +2,12 @@
 
 namespace MediaWiki\Extension\JsonConfig\Tests;
 
+use MediaWiki\Extension\Scribunto\Tests\Engines\LuaCommon\LuaEngineTestBase;
+
+if ( !class_exists( LuaEngineTestBase::class ) ) {
+	return;
+}
+
 /**
  * @group Lua
  * @group LuaStandalone
